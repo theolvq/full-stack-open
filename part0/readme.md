b5982c116355f001db75449b8e57a7bd18f47d0a
daawascript <70675996+daawascript@users.noreply.github.com>
2021-05-23 19:38:06 -0700

    completed part1d exercises and adjusted readme for part0

--- part0/readme.md
+++ part0/readme.md
@@ -1,3 +1,6 @@
 # Part0 of Full Stack Open 
 Assignments can be found here: https://fullstackopen.com/en/part0/fundamentals_of_web_apps#exercises-0-1-0-6
 
+## Key takeaways
+1. Old school web apps request all the files every time there is a change being made such as a form submit
+2. Single Page Apps (SPA) do not, they only update the data via HTTP POST
---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(
+    Array.from({ length: anecdotes.length }, () => 0)
+  );
+
+  const handleNext = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+
+  const handleVote = () => {
+    setPoints(points => {
+      const newPoints = [...points];
+      newPoints[selected] += 1;
+      return newPoints;
+    });
+  };
+
+  const winner = anecdotes[points.indexOf(Math.max(...points))];
+
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
+      <p>{anecdotes[selected]}</p>
+      <p>
+        This anecdote has {points[selected]} vote
+        {points[selected] > 1 ? 's' : ''}
+      </p>
+      <button onClick={handleNext}>next anecdote</button>
+      <button onClick={handleVote}>vote</button>
+      <h2>Anecdote with most votes</h2>
+      <p>{winner}</p>
+    </div>
+  );
+};
+
+export default App;
