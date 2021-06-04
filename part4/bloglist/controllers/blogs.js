92c8d7062bee4093970eb43330bcd3e1300c2371
daawascript <70675996+daawascript@users.noreply.github.com>
2021-06-04 16:04:24 -0700

    controller and model working

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,49 @@
+const blogRouter = require('express').Router();
+const Blog = require('../models/blog');
+
+blogRouter.get('/', (req, res) => {
+  res.json({ message: 'Server Working' });
+});
+
+blogRouter.get('/', (req, res) => {
+  Blog.find({})
+    .then(blogs => {
+      res.json(blogs);
+    })
+    .catch(err => console.log(err));
+});
+
+blogRouter.get('/:id', (req, res) => {
+  Blog.findById(req.params.id)
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+blogRouter.post('/', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+blogRouter.delete('/:id', (req, res) => {
+  Blog.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+module.exports = blogRouter;
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -4,19 +4,11 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const blogRouter = require('./controllers/blogs');
 require('dotenv').config();
 
 morgan.token('body', req => JSON.stringify(req.body));
 app.use(morgan(':method :url :response-time ms \n :body'));
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
 
 const mongoUrl = process.env.MONGODB_URI;
 
@@ -27,61 +19,10 @@ mongoose.connect(mongoUrl, {
   useCreateIndex: true,
 });
 
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Server Working' });
-});
-
-app.get('/api/blogs', (req, res) => {
-  Blog.find({})
-    .then(blogs => {
-      res.json(blogs);
-    })
-    .catch(err => console.log(err));
-});
-
-app.get('/api/blogs/:id', (req, res) => {
-  Blog.findById(req.params.id)
-    .then(result => {
-      res.status(200).json(result);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then(result => {
-      res.status(201).json(result);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.delete('/api/blogs/:id', (req, res) => {
-  Blog.findByIdAndRemove(req.params.id)
-    .then(() => {
-      res.status(204).end();
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
+app.use('/api/blogs', blogRouter);
 
 const PORT = process.env.PORT;
 
