a711749da2de7943aca16369cee3b7bbfab1c024
daawascript <70675996+daawascript@users.noreply.github.com>
2021-06-11 16:39:23 -0700

    linked users and notes, completed 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   res.json(blogs);
 });
 
@@ -12,8 +13,19 @@ blogRouter.get('/:id', async (req, res) => {
 });
 
 blogRouter.post('/', async (req, res) => {
-  const blog = new Blog(req.body);
+  const body = req.body;
+  const user = await User.findById(body.userId);
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id,
+  });
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
   res.status(201).json(savedBlog);
 });
 
--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -30,7 +30,6 @@ usersRouter.get('/', async (req, res) => {
   const users = await User.find({}).populate('blogs', {
     title: 1,
     author: 1,
-    url: 1,
     likes: 1,
   });
   res.json(users);
